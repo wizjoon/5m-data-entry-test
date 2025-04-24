8f8b5a5d2f1d0ce2213b17a25058f7ba5af8f3e3
wizjoon <36699135+wizjoon@users.noreply.github.com>
2025-04-24 23:32:40 +0800

    Update q5.py

--- src/q5.py
+++ src/q5.py
@@ -15,11 +15,24 @@ def check_divisibility(num, divisor):
 
 # Task 2
 # Invoke the function "check_divisibility" using the following scenarios:
-# - 10, 2
-# - 7, 3
 
+
+# Test 1: 10, 2
 print(check_divisibility(10, 2))  # Expected output: True
+
+# Test 2: 7, 3
 print(check_divisibility(7, 3))   # Expected output: False
 
+# Test 3: num = 0
 print(check_divisibility(7, 0))   # Expected output: False
+
+# Test 4: divisor = 0
 print(check_divisibility(0, 3))   # Expected output: False
+
+
+# Test 5: num is not a number
+print(check_divisibility("a", 3))   # Expected output: False
+
+# Test 6: divisor is not a number
+print(check_divisibility(10, "a"))   # Expected output: False
+
