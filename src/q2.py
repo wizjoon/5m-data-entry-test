7fa191d46111001f64a142306491a30bec4a6ca6
wizjoon <36699135+wizjoon@users.noreply.github.com>
2025-04-25 00:33:31 +0800

    Update q2.py

--- src/q2.py
+++ src/q2.py
@@ -5,9 +5,8 @@ def find_and_replace(lst, find_val, replace_val):
     - lst must be a list.
     - Return the modified list.
     """
-  
     if not isinstance(lst, list):
-        raise ValueError("Input must be a list.")
+        return "Input must be a list."
 
     return [replace_val if item == find_val else item for item in lst]
 
@@ -16,10 +15,19 @@ def find_and_replace(lst, find_val, replace_val):
 # Invoke the function "find_and_replace" using the following scenarios:
 
 
-# - [1, 2, 3, 4, 2, 2], 2, 5
+# Test 1: [1, 2, 3, 4, 2, 2], 2, 5
+# Expected Output: [1, 5, 3, 4, 5, 5]
 print(find_and_replace([1, 2, 3, 4, 2, 2], 2, 5))
 
-# - ["apple", "banana", "apple"], "apple", "orange"
+# Test 2: ["apple", "banana", "apple"], "apple", "orange"
+# Expected Output: ['orange', 'banana', 'orange']
 print(find_and_replace(["apple", "banana", "apple"], "apple", "orange"))
 
-print(find_and_replace(["apple", "banana", "apple"], "pineapple", "orange"))
+# Test 3: no input list
+# Expected Output: Input must be a list.
+print(find_and_replace("apple", "apple", "orange"))
+
+# Test 4: ("apple", "banana", "apple"), "apple", "orange", list is a tuple instead of list
+# Expected Output: Input must be a list.
+print(find_and_replace(("apple", "banana", "apple"), "apple", "orange"))
+
