4dcb3cae08eac034e9f1d4578fb32a8106666f48
wizjoon <36699135+wizjoon@users.noreply.github.com>
2025-04-24 15:15:28 +0800

    Update q2.py

--- src/q2.py
+++ src/q2.py
@@ -5,10 +5,21 @@ def find_and_replace(lst, find_val, replace_val):
     - lst must be a list.
     - Return the modified list.
     """
-    return
+  
+    if not isinstance(lst, list):
+        raise ValueError("Input must be a list.")
+
+    return [replace_val if item == find_val else item for item in lst]
 
 
 # Task 2
 # Invoke the function "find_and_replace" using the following scenarios:
+
+
 # - [1, 2, 3, 4, 2, 2], 2, 5
+print(find_and_replace([1, 2, 3, 4, 2, 2], 2, 5))
+
 # - ["apple", "banana", "apple"], "apple", "orange"
+print(find_and_replace(["apple", "banana", "apple"], "apple", "orange"))
+
+print(find_and_replace(["apple", "banana", "apple"], "pineapple", "orange"))
