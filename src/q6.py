f6d65ed7a5bff394d55a0aaca611348460c86e9e
wizjoon <36699135+wizjoon@users.noreply.github.com>
2025-04-24 21:26:58 +0800

    Update q6.py

--- src/q6.py
+++ src/q6.py
@@ -5,10 +5,18 @@ def find_first_negative(lst):
     - Return the first negative number if found, otherwise return "No negatives".
     - Use a while loop to implement this.
     """
-    return
+    i = 0
+    while i < len(lst):
+        if lst[i] < 0:
+            return lst[i]
+        i += 1
+    return "No negatives"
 
 
 # Task 2
 # Invoke the function "find_first_negative" using the following scenario:
 # - [3, 5, -1, 7, -2, 8]
 # - [2, 10, 7, 0]
+
+print(find_first_negative([3, 5, -1, 7, -2, 8]))  # Expected: -1
+print(find_first_negative([2, 10, 7, 0]))         # Expected: "No negatives"
